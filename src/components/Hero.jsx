8a7423f957eda7355fbfde93541adee5330e8c72
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2025-05-26 19:42:20 +0530

    add changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import img1 from '../assets/myp.jpg'
 
-
 function Hero() {
     return (
         <>
             <div className='h-m-screen w-screen flex iteam-center flex-wrap  justify-center iteam-center  mt-10 '>
                 <div className='h-[450px] mx-auto sm:w-[300px]  w-[250px] bg-red-600 rounded-b-full overflow-hidden '>
-                    <img src={img1} alt="profile" className='h-[100vh] w-full rounded-b-full object-cover ' />
+                 <img src={img1} alt="profile" className='h-[100vh] w-full rounded-b-full object-cover ' />
                 </div>
 
                 <div className='h-full w-full md:w-[60vw] justify-center iteam-center pt-10 mt-10  '>
@@ -15,11 +14,9 @@ function Hero() {
                         <h4 className='text-5xl sm:text-7xl text-center '>A Creative <span className='text-green-500 font4 text-6xl sm:text-8xl'>Developer</span><br></br><span className='font4 text-6xl sm:text-8xl text-green-500'>&</span>digital teacher</h4>
                     </div>
                     <p className='text-center'>I collaborate with brands globally to design impactful, mission-focused websites that drive results and achieve business goals.</p>
-                    <div className='cursor-pointer overflow-hidden'>
-                        
-                    </div>
+                    <button className='py-3  px-10 bg-black text-white rounded-4xl cursor-pointer'>My Resume</button>
                 </div>
-
+                
 
 
             </div>
